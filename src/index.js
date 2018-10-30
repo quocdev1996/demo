936b7e575837d398c3016f3b2759b7fdb4b9eb6a
nguyen sinh <nguyensinh@nguyens-iMac-2.local>
2018-10-30 15:57:33 +0700

    Release version 1.0.0

---
+++ src/index.js
@@ -0,0 +1,14 @@
+ 
+ 
+window.onload = function()
+{
+    console.log('logooooo--------------->');
+    do_validate();
+};
+var do_validate = prompt("Nhập tên của bạn", '');
+alert(t);
+
+var element = document.getElementById('demo');
+ 
+// Lấy giá trị của thẻ input
+document.write('Tao tao bi dao');
